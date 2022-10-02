83f954fb19d7b9649e0df656843391f4fb6d07d2
Ricky Bakersmith <90644027+RickyB923@users.noreply.github.com>
2022-10-01 22:44:32 -0500

    LoopManager

---
+++ Assets/Scripts/LoopManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoopManager : MonoBehaviour
+{
+    [SerializeField] float loopTime;
+    [SerializeField] string[] scenes;
+    private int sceneNumber;
+    void Start()
+    {
+        Debug.Log($"This scene is number {SceneManager.GetActiveScene().buildIndex}");
+        //Debug.Log(SceneManager.sceneCountInBuildSettings);
+
+        sceneNumber = SceneManager.GetActiveScene().buildIndex + 1;
+        if(sceneNumber == SceneManager.sceneCountInBuildSettings)
+        {
+            sceneNumber = 0;
+        }
+        StartCoroutine(Wait(loopTime, sceneNumber));
+    }
+
+    IEnumerator Wait(float seconds, int i)
+    {
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("done");
+        SceneManager.LoadScene(scenes[i]);
+    }
+}
