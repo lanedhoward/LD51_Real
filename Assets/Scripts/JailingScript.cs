7c4199df28db0e80284f360f7e9e78f49e18daa0
Ricky Bakersmith <90644027+RickyB923@users.noreply.github.com>
2022-10-03 12:38:57 -0500

    police scene interactions set up

--- Assets/Scripts/JailingScript.cs
+++ Assets/Scripts/JailingScript.cs
@@ -7,15 +7,30 @@ public class JailingScript : MonoBehaviour
     [SerializeField] PlayerController player;
     [SerializeField] Transform[] waypoints;
     [SerializeField] float speed;
+    private CapsuleCollider2D playerCollider;
     private int currentWaypoint = 0;
+    private bool timeToMovePlayer;
 
+    void Start()
+    {
+        playerCollider = player.GetComponent<CapsuleCollider2D>();
+    }
     void Update()
     {
         bool check;
-        if(player.interactionsInventory.inventory.TryGetValue("tookRobbersGun", out check) && check == true)
+        if(player.interactionsInventory.inventory.TryGetValue("jailed", out check) && check == true)
         {
-            CheckPosition();
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed);
+            if(currentWaypoint < waypoints.Length)
+            {
+                CheckPosition();
+                transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed);
+                if(timeToMovePlayer)
+                {
+                    playerCollider.enabled = false;
+                    player.transform.position = Vector3.MoveTowards(player.transform.position, waypoints[1].position, speed);
+                }
+            }
+            
         }
     }
     void CheckPosition()
@@ -23,6 +38,15 @@ public class JailingScript : MonoBehaviour
         if(transform.position == waypoints[currentWaypoint].position)
         {
             currentWaypoint++;
+        }
+        if(transform.position == player.transform.position)
+        {
+            timeToMovePlayer = true;
+        }
+        if(player.transform.position == waypoints[2].position)
+        {
+            playerCollider.enabled = true;
+            timeToMovePlayer = false;
         }
     }
 }
